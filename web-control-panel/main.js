2befc623a667c8f59ca700a7767bc14d18aeeb92
MikeS <MikeS@example.com>
2026-07-18 12:11:07 -0500

    Fix camera Z math

--- web-control-panel/main.js
+++ web-control-panel/main.js
@@ -142,7 +142,7 @@ function fitCameraToObject(camera, object, offset = 1.25) {
     
     const maxDim = Math.max(size.x, size.y, size.z);
     const fov = camera.fov * (Math.PI / 180);
-    let cameraZ = Math.abs(maxDim / 2 * Math.tan(fov * 2));
+    let cameraZ = Math.abs((maxDim / 2) / Math.tan(fov / 2));
     
     cameraZ *= offset;
     
